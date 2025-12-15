d2b27cebaec9b446458c1e5ff08627d0d183f2a2
Batot_BooB <137377427+Batot-boop@users.noreply.github.com>
2025-12-15 04:57:34 +0200

    Add Round Robin Scheduler

--- src/schedulers/RRScheduler.java
+++ src/schedulers/RRScheduler.java
@@ -1,5 +1,77 @@
 package src.schedulers;
+import java.util.*;
+import src.models.Process;
 
 public class RRScheduler {
-    
+    public static List<String> run(List<Process> processes, int quantum, int contextSwitch) {
+        // Sort processes by arrival time
+        processes.sort(Comparator.comparingInt(p -> p.arrivalTime));
+        
+        List<String> executeOrder = new ArrayList<>();
+        Queue<Process> readyQueue = new LinkedList<>();
+        
+        int currentTime = 0;
+        int completeProcess = 0;
+        int numProcess = processes.size();
+
+        // To Check if processes arrived
+        boolean[] arrived = new boolean[numProcess];
+
+        while(completeProcess < numProcess) {
+
+            // Add all processes are arrived now
+            for(int i = 0; i < numProcess; i++) {
+                Process p = processes.get(i);
+                if(!arrived[i] && p.arrivalTime <= currentTime) {
+                    readyQueue.add(p);
+                    arrived[i] = true;
+                }
+            }
+
+            // If no process is ready, jump to next arrival
+            if(readyQueue.isEmpty()) {
+                currentTime++;
+                continue;
+            }
+            
+            // Pick First process from ready queue
+            Process currentProcessName = readyQueue.poll();
+
+            executeOrder.add(currentProcessName.name);
+
+            // Execute for quantum or remaining time
+            int execTime = Math.min(quantum, currentProcessName.remainingTime);
+
+            // Simualate execution
+            for(int j = 0; j < execTime; j++) {
+                currentTime++;
+
+                // Check for newly arrived processes during execution
+                for(int i = 0; i < numProcess; i++) {
+                    Process p = processes.get(i);
+                    if(!arrived[i] && p.arrivalTime <= currentTime) {
+                        readyQueue.add(p);
+                        arrived[i] = true;
+                    }
+                }
+            }
+
+            // Deduct executed time from remaining time
+            currentProcessName.remainingTime -= execTime;
+
+            // If process is completed
+            if(currentProcessName.remainingTime == 0) {
+                completeProcess++;
+                currentProcessName.turnaroundTime = currentTime - currentProcessName.arrivalTime;
+                currentProcessName.waitingTime = currentProcessName.turnaroundTime - currentProcessName.burstTime;
+            } else {
+                // Re-add to the end of the queue if not completed
+                readyQueue.add(currentProcessName);
+            }
+
+            // Add context switch time
+            currentTime += contextSwitch;
+        }
+        return executeOrder;
+    }
 }
